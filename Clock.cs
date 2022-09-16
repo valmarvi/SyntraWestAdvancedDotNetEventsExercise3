4d83f4dfaf07726de7b29cfb6da7456bda853173
Valter Vianês <valter.vianes@gmail.com>
2022-09-16 21:35:50 +0200

    Add project files.

---
+++ Clock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace AdvancedDotNetEventsExercise3
+{
+    public class Clock
+    {
+        private DateTime _alarmTime;
+        private Timer _timer;
+        public event EventHandler Alarm;
+
+        public Clock(DateTime alarmTime)
+        {
+            _alarmTime = alarmTime;
+            _timer = new Timer(TimerTriggered, null, 0, 1000);
+        }
+
+        public void TimerTriggered(object? s)
+        {
+            if (_alarmTime.ToString() == DateTime.Now.ToString())
+            {
+                Alarm?.Invoke(this, new EventArgs());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdvancedDotNetEventsExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clock = new Clock(new DateTime(2022, 9, 15, 20, 43, 25));
+
+            clock.Alarm += Clock_Alarm;
+
+            void Clock_Alarm(object sender, EventArgs e)
+            {
+                Console.WriteLine("Alarm goes off;");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
